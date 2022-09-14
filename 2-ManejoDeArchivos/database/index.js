49e22a5c1f27f4138264944713d05200537f09e4
Axel-Ramos <axelramoscir@gmail.com>
2022-09-14 19:01:25 -0300

    Entrega Manejo de Archivos

---
+++ 2-ManejoDeArchivos/database/index.js
@@ -0,0 +1,22 @@
+const productos =[
+    {
+        id:"1",
+        nombre:"Cuchara Helado",
+        precio:"100",
+        imagen:"/imagenes/cuchara1.jpeg"
+    },
+    {
+        id:"2",
+        nombre:"Cuchara Postre",
+        precio:"110",
+        imagen:"/imagenes/cuchara2.jpeg"
+    },
+    {
+        id:"3",
+        nombre:"Máscara Facial",
+        precio:"300",
+        imagen:"/imagenes/mascara-facial1.jpeg"
+    }
+];
+
+module.exports = productos;
---
+++ 2-ManejoDeArchivos/index.js
@@ -0,0 +1,10 @@
+const Container = require('./class');
+const data = require('./database');
+
+const productos = new Container('./database.txt');
+
+productos.save(data);
+productos.getById(2);
+productos.getAll();
+productos.deleteById(2);
+productos.deleteAll();
